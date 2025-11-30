ebdc28e0700d29bf7047e3b2debd56548a16b7a6
ム s̷  ᑢ Ｅ <107835078+Portgasu-D-Asce@users.noreply.github.com>
2025-11-30 18:36:03 +0530

    Update description and improve player check

--- commands/list_players.js
+++ commands/list_players.js
@@ -2,14 +2,15 @@ import { listOnlinePlayers } from "../utils/pebble.js";
 
 export default {
   name: "list_players",
-  description: "Show players currently online",
+  description: "Show online players",
 
   async execute(interaction) {
     const data = await listOnlinePlayers();
     const players = data?.players || [];
 
-    if (players.length === 0)
+    if (players.length === 0) {
       return interaction.reply("👥 No players online.");
+    }
 
     return interaction.reply("👥 Online Players:\n" + players.join("\n"));
   }
